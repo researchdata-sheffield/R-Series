b7cf82da7deb9d43339473a688b00c62dcf46640
yld-weng <59968766+yld-weng@users.noreply.github.com>
2021-01-25 18:18:22 +0000

    update

---
+++ part3/other.R
@@ -0,0 +1,19 @@
+library(highcharter)
+
+source('./climateData.R')
+
+
+shefClimateDf <- shefClimateNoNA %>%
+  mutate(
+    season = year(TIMESTAMP)
+  ) %>%
+  filter(
+    between(TIMESTAMP, as_datetime("2012-01-01"), as_datetime("2013-12-31"))
+  ) %>%
+  mutate(
+    TIMESTAMP = format.Date(TIMESTAMP, "%m-%d")
+  )
+
+hchart(shefClimateDf, "line", hcaes(x = TIMESTAMP, y = AirTC_Avg, group = season))
+
+
